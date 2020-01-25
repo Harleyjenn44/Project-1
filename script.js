daaae4e74eeb69d93a0b6bb423d388bfa253c818
YKTV <bpbouvilom@yahoo.com>
2020-01-25 11:01:03 -0600

    merging with master

--- script.js
+++ script.js
@@ -13,3 +13,54 @@
            }
        });
    }
+
+   // Random GIF generator for story
+   $(document).ready(function() {
+   var refresh; 
+   const duration = 1000 * 10;
+   const giphy = {
+       baseURL: "https://giphy.com/search/lord-of-the-rings" & "https://giphy.com/search/game-of-thrones",  
+        apiKey: "VlrpoAjzBBhIekrh7YojpXOhXG4raUNk",
+        tag: "gameOfRings",
+        type: "random", 
+        rating: "pg-13"
+   };
+
+   const $gif_wrap = $("#gif-wrap");
+
+   let giphyURL = encodeURI(
+       giphy.baseURL +
+       giphy.type +
+       "?api_key=" +
+       giphy.apiKey +
+       "&tag=" +
+       giphy.tag +
+       "&rating=" +
+       giphy.rating
+   );
+
+   // Call giphy API and render data 
+
+   var newGif = () => $.getJSON(giphyURL,
+    json => renderGif(json.data));
+
+    // Display Gif in gif container 
+
+    var renderGif = _giphy => {
+        console.log(_giphy);
+        // set gif as bg image
+        $gif_wrap.css({
+            "background-image": 'url("' + 
+            _giphy.imgage_original_url + '")'
+        });
+
+        // start duration countdown
+        // refreshRate();
+    };
+
+    newGif();
+
+    const newGifButton = $('#new-gif');
+
+    newGifButton.click(newGif) 
+}); 
