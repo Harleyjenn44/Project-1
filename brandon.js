7f2926aa78439b9de378fc1730fe322d6185ffaa
YKTV <bpbouvilom@yahoo.com>
2020-01-29 16:27:14 -0600

    changed brandon JS

--- brandon.js
+++ brandon.js
@@ -1,19 +1,36 @@
  /* random gif generator function */
-var api = 'https://api.giphy.com/v1/gifs/search?';
-var apiKey = '&api_key=XSXt8Mw82KruMzNHClWNR3VRLvP3nTMO';
-var query = ['&q=lord+of+the+rings', '&q=game+of+thrones'];
-var limit = '&limit=1'
-var btn = 'render'
-function setup() {
-  noCanvas();
-  var url = api + apiKey + query;
-  loadJSON(url, gotData);
+ const gif = "https://api.giphy.com/v1/gifs/search?api_key=XSXt8Mw82KruMzNHClWNR3VRLvP3nTMO&q=game+of+thrones&limit=2&rating=PG";
+const giphy= {
+  apiBase: "https://api.giphy.com/v1/gifs/search?",
+  apiKey: "XSXt8Mw82KruMzNHClWNR3VRLvP3nTMO",
+  query: 'lord+of+the+rings' & "game+of+thrones", 
+  limit: "1",
+  rating: "PG"
+};
+//Target containter where gif is
+const $gifWrap = $(".gifWrap");
+// Giphy API URL
+let giphyApi = encodeURI(
+  giphy.baseURL +
+  "api_key" +
+  giphy.apiKey +
+  "&q=" +
+  giphy.query + 
+  "&limit=" +
+  giphy.limit + 
+  "&rating" +
+  giphy.rating
+);
+
+//Call giphy API and render data when "onward" button is pressed
+var render = () => $.getJSON(gif, json => renderGif(json.data));
+
+//Display gif in container
+var renderGif = _giphy => {
+  console.log(_giphy);
 }
 
-console.log(setup);
+var btn = document.getElementById("render");
 
-function gotData(data) {
-  println(data.data[0].images.original.url);
-}
-
-console.log(data);
+btn.onclick = function() {
+  renderGif}; 
