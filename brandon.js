443b0f6f58b905cff867a0482adde8ef37822a37
YKTV <bpbouvilom@yahoo.com>
2020-01-29 12:14:41 -0600

    updated JS

--- brandon.js
+++ brandon.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-    $("#new").click(function() {
-      var imgSrc;
-      var imgId;
-      $.ajax ({
-        url: "api.giphy.com/v1/gifs/search",
-        type: "GET",
-        dataType: "json",
-        success: function(response) {
-          imgSrc = response.data.image_url;
-          imgId = response.data.id;
-          console.log(response);
-          $("#giphy-embed").attr("src", "https://giphy.com/embed/"+imgSrc); 
-          }
-        });
-    });
-  });
+ /* random gif generator function */
+var api = 'https://api.giphy.com/v1/gifs/search?';
+var apiKey = '&api_key=XSXt8Mw82KruMzNHClWNR3VRLvP3nTMO';
+var query = ['&q=lord+of+the+rings', '&q=game+of+thrones'];
+var limit = '&limit=1'
+var btn = 'render'
+function setup() {
+  noCanvas();
+  var url = api + apiKey + query;
+  loadJSON(url, gotData);
+}
+
+console.log(setup);
+
+function gotData(data) {
+  println(data.data[0].images.original.url);
+}
+
+console.log(data);
